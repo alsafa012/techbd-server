108bade0b06766efb121451324f3e5f36d5a9091
alsafa012 <alsafa012@gmail.com>
2023-10-19 20:05:17 +0600

    data remove from database functionality added

--- index.js
+++ index.js
@@ -65,8 +65,12 @@ async function run() {
                const result = await addCardCollection.insertOne(product);
                res.send(result);
           });
-          
-
+          app.delete("/addCart/:id",async(req,res)=>{
+               const id = req.params.id;
+               const query = {_id: new ObjectId(id)};
+               const result = await addCardCollection.deleteOne(query);
+               res.send(result);
+          })
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
           console.log(
